28c6f1ab108a92f36297c9b7b4e8b20355412a36
cootshk <83678457+cootshk@users.noreply.github.com>
2023-11-01 21:15:06 -0500

    Move definitions to a new file

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,30 +2,6 @@
 """
 
 from ..main import Table
-if __name__ == "__main__": # definitions
-    print("Table definitions:")
-    #print(f"{Table() = }")
-    print(f"{repr(Table()) = }")
-    print(f"{str(Table()) = }")
-    #print(f"{Table(1, 2, 3) = }")
-    print(f"{repr(Table(1, 2, 3)) = }")
-    print(f"{str(Table(1, 2, 3)) = }")
-    #print(f"{Table(foo='bar', spam='eggs') = }")
-    print(f"{repr(Table(foo='bar', spam='eggs')) = }")
-    print(f"{str(Table(foo='bar', spam='eggs')) = }")
-    #print(f"{Table(1, 2, 3, foo='bar', spam='eggs') = }")
-    print(f"{repr(Table(1, 2, 3, foo='bar', spam='eggs')) = }")
-    print(f"{str(Table(1, 2, 3, foo='bar', spam='eggs')) = }")
-    print(f"{len(Table()) = }") # 0
-    print(f"{len(Table(1, 2, 3)) = }") # 3
-    print(f"{len(Table(foo='bar', spam='eggs')) = }") # 2
-    print(f"{len(Table(1, 2, 3, foo='bar', spam='eggs')) = }") # 5
-    print(f"{Table(1,2,3).list = }") # [1,2,3]
-    print(f"{Table(1,2,3).dict = }") # {}
-    print(f"{Table(foo='bar', spam='eggs').list = }") # []
-    print(f"{Table(foo='bar', spam='eggs').dict = }") # {'foo': 'bar', 'spam': 'eggs'}
-    print(f"{Table(1,2,3, foo='bar', spam='eggs').foreach((lambda _, y: y ), True, False) = }")
-    exit(0)
 
 def test_tables():
     """Unit tests for Table() class
