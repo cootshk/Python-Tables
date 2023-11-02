4ba0e4d11ea0378585988cc73f73efb0c650031c
cootshk <83678457+cootshk@users.noreply.github.com>
2023-11-01 21:11:47 -0500

    v1.1.0

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-#check for python version
+"""The Table module.
+
+    See the Table class for more info.
+
+    """
+
+# check for python version
+from typing import Iterable, override, Any, Optional, overload, Callable
 import sys
 if sys.version_info < (3, 12):
     print("This script requires Python 3.12+")
     exit(1)
 del sys
-from typing import Iterable, override, Any, Optional, overload, Callable
+
+
 class Table(Iterable):
+    """The Table class. It's a list and a dict combined.
+
+    Examples:
+        Table() # Table([])
+        Table(1, 2, 3) # Table([1, 2, 3])
+        Table(foo="bar", spam="eggs") # Table({'foo': 'bar', 'spam': 'eggs'})
+        # Table([1, 2, 3]; {'foo': 'bar', 'spam': 'eggs'})
+        Table(1, 2, 3, foo="bar", spam="eggs")
+        Table([], {}) # Table([])
+        Table([1, 2, 3], {}) # Table([1, 2, 3])
+        Table(1,2,3) == Table([1,2,3]) # True
+        len(Table("a", "b", "c", four="d", five="e")) # 5
+    """
     type KeyValue = str | int
-    #typing
+    # typing
     @overload
     def __init__(self): ...
     @overload
-    def __init__(self, list: list[Any],/): ...
+    def __init__(self, list_val: list[Any], /): ...
     @overload
-    def __init__(self, dict: dict[str, Any],/): ...
+    def __init__(self, dict_val: dict[str, Any], /): ...
     @overload
-    def __init__(self, list: list[Any], dict: dict[str, Any],/): ...
+    def __init__(self, list_val: list[Any], dict_val: dict[str, Any], /): ...
     @overload
     def __init__(self, *list_values: Optional[Any]): ...
     @overload
     def __init__(self, **dict_values: Optional[Any]): ...
+
     @overload
-    def __init__(self, *list_values: Optional[Any], **dict_values: Optional[Any]): ...
+    def __init__(self, *list_values: Optional[Any], **
+                 dict_values: Optional[Any]): ...
 
     @overload
     def __delitem__(self, key: KeyValue): ...
@@ -35,19 +58,66 @@ class Table(Iterable):
 
     @overload
     def find_keys(self, value: Any, /) -> KeyValue: ...
+
     @overload
-    def find_keys[default: Any](self, value: Any,/,*, default:default=None) -> list[KeyValue] | default: ...
+    def find_keys[default: Any](
+        self, value: Any, /, *, default: Any = None) -> list[KeyValue] | default: ...
+
+    @overload
+    def sort(self) -> "Table": ...
+    @overload
+    def sort(self, *, key: Callable) -> "Table": ...
+    @overload
+    def sort(self, *, reverse: bool) -> "Table": ...
+    @overload
+    def sort(self, *, key: Callable, reverse: bool) -> "Table": ...
+
     ###############################################
     # Types:
     # self.list: list[Any]
     # self.dict: dict[str, Any]
     # KeyValue: str | int
-    # TODO: Ensure that all lines have type hinting
+
+    # Functions:
+    # __init__
+    # __getitem__
+    # __setitem__
+    # __delitem__
+    # __iter__
+    # __len__
+    # _list_len_
+    # _dict_len_
+    # __repr__
+    # _unpack_kwargs_to_str_
+    # __str__
+    # __contains__
+    # find_keys
+    # append
+    # extend
+    # insert
+    # pop
+    # remove
+    # clear
+    # copy
+    # count
+    # foreach
+    # __eq__
+    # __add__
+    # sort
     ###############################################
 
     @override
     def __init__(self, *args: Optional[Any], **kwargs: Optional[Any]):
-        if ( #Table([], {})
+        """The init function. 
+        Args:
+            *list_values (list[Any] | Any): The list values. If there is only one argument, then that is set to the list.
+            **dict_values (dict[str, Any] | Any): The dict values. If there is only one argument, then that is set to the dict.
+        
+        Examples:
+            see the Table() documentation
+
+        """
+        if (  # Table([], {})
             len(args) == 2 and
             len(kwargs) == 0 and
             isinstance(args[0], list) and
@@ -57,7 +127,7 @@ class Table(Iterable):
             self.dict = args[1]
             return
 
-        elif ( #Table([])
+        elif (  # Table([])
             len(args) == 1 and
             len(kwargs) == 0 and
             isinstance(args[0], list)
@@ -65,7 +135,7 @@ class Table(Iterable):
             self.list = list[Any](args[0])
             self.dict = dict[str, Any]({})
             return
-        elif ( #Table({})
+        elif (  # Table({})
             len(args) == 1 and
             len(kwargs) == 0 and
             isinstance(args[0], dict)
@@ -73,21 +143,25 @@ class Table(Iterable):
             self.list = list[Any]([])
             self.dict = dict[str, Any](args[0])
             return
-        #table(1, 2, 3, foo="bar", spam="eggs", ...)
+        # table(1, 2, 3, foo="bar", spam="eggs", ...)
         self.list = list(args)
         self.dict = dict[str, Any](kwargs)
+
     def __getitem__(self, key: KeyValue) -> Any:
         return self.dict[key] if isinstance(key, str) else self.list[key]
+
     def __setitem__(self, key: KeyValue | Iterable[KeyValue], value: Any):
-        if isinstance(key, str):    
+        if isinstance(key, str):
             self.dict[key] = value
         elif isinstance(key, int):
             self.list[key] = value
         elif isinstance(key, Iterable):
-            for i, j, k in zip(key, value, range(len(value))):
-                self.__setitem__(i, value)
+            for i, j, _ in zip(key, value):
+                self[i] = j
         else:
-            raise TypeError(f"Table indices must be integers or strings, not {type(key).__name__}")
+            raise TypeError(f"Table indices must be integers or strings, not {
+                            type(key).__name__}")
+
     def __delitem__(self, key: KeyValue | Iterable[KeyValue]):
         if isinstance(key, Iterable):
             for i in key:
@@ -107,34 +181,59 @@ class Table(Iterable):
                 else:
                     raise KeyError(f"Index {key} not found")
             case _:
-                raise TypeError(f"Table indices must be integers or strings, not {type(key).__name__}")
+                raise TypeError(f"Table indices must be integers or strings, not {
+                                type(key).__name__}")
+
     @override
     def __iter__(self) -> Any:
         return iter(self.list)
+
     def __len__(self) -> int:
         return len(self.list) + len(list(self.dict.keys()))
+
     def _list_len_(self) -> int:
         return len(self.list)
+
     def _dict_len_(self) -> int:
         return len(self.dict)
+
     @override
     def __repr__(self) -> str:
         if len(self) == 0:
             return "Table()"
-        return f"Table({self.list}; {self.dict})".replace("[]; ", "").replace("; {}", "")
+        return f"Table({self.list}; {self.dict})".replace(
+            "[]; ", "").replace(
+                "; {}", "")
+
     def _unpack_kwargs_to_str_(self) -> str:
         ret = ""
         for k, v in zip(self.dict.keys(), self.dict.values()):
-            ret += f"{k}:{v if not isinstance(v, str) else f"'{v}'"}, "
+            ret += f"{k}: {v if not isinstance(v, str) else f"'{v}'"}, "
         return ret.removesuffix(", ")
+
     @override
     def __str__(self) -> str:
-        #if len(self) == 0:
+        # if len(self) == 0:
         #    return "<>"
-        return f"<{(''.join([f'{x}, ' for x in self.list])).removesuffix(', ')}, {self._unpack_kwargs_to_str_()}>".replace("<, ", "<").replace(", >",">")
+        return f"<{(''.join([
+            f'{x}, ' for x in self.list
+        ])).removesuffix(', ')}, {
+                self._unpack_kwargs_to_str_()}>".replace(
+                    "<, ", "<").replace(
+                        ", >", ">")
+
     def __contains__(self, key: str) -> bool:
         return (key in self.dict) or (key in self.list)
-    def find_keys(self, value: Any, /, *, default:Any=None) -> list[KeyValue] | Any:
+    def find_keys(self, value: Any, /, *, default: Any=None) -> list[KeyValue] | Any:
+        """Finds all keys and indices that have a value equal to value.
+
+        Args:
+            value (Any): The value to search for.
+            default (Any, optional): What to return if no matches are found. Defaults to None.
+
+        Returns:
+            list[KeyValue] | Any: A list of all keys an indicies, otherwise default.
+        """
         ret = []
         for i in self.list:
             if i == value:
@@ -147,37 +246,140 @@ class Table(Iterable):
         elif len(ret) == 1:
             return ret[0]
         return ret
-    def append(self, value: Any):
+
+    @override
+    def append(self, value: Any) -> "Table":
+        """Append a value to the end of the list.
+
+        Args:
+            value (Any): The value to insert.
+
+        Returns:
+            Table: self. Useful for chaining and repls.
+        """
         self.list.append(value)
-    def extend(self, value: Iterable[Any]):
+        return self
+
+    @override
+    def extend(self, value: Iterable[Any]) -> "Table":
+        """Extend the list with another iterable.
+
+        Args:
+            value (Iterable[Any]): The other iterable.
+
+        Returns:
+            Table: self. Useful for chaining and repls.
+        """
         self.list.extend(value)
-    def insert(self, index: int, value: Any):
+        return self
+
+    @override
+    def insert(self, index: int, value: Any) -> "Table":
+        """Insert a value at a specific index.
+
+        Args:
+            index (int): the index to insert at.
+            value (Any): the value to insert.
+
+        Returns:
+            Table: self. Useful for chaining and repls.
+        """
         self.list.insert(index, value)
-    def pop(self, index: int=-1) -> Any:
+        return self
+
+    @override
+    def pop(self, index: int = -1) -> Any:
+        """Returns a specific value in a list and removes it.
+
+        Args:
+            index (int, optional): The value to return. Defaults to -1.
+
+        Returns:
+            E: The item that was removed.
+        """
         return self.list.pop(index)
-    def remove(self, value: Any):
+
+    def remove(self, value: Any) -> "Table":
+        """Removes an item from the list, if it exists.
+
+        Args:
+            value (Any): The value to remove.
+
+        Returns:
+            Table: self. Useful for chaining and repls.
+        """
         self.list.remove(value)
-    def clear(self):
+        return self
+
+    def clear(self) -> "Table":
+        """Makes the table empty.
+
+        Returns:
+            Table: An empty table. Equivalent to Table().
+        """
         self.list.clear()
         self.dict.clear()
+        return self
+
     def copy(self) -> "Table":
+        """Makes a second copy of the table. Useful to deal with references.
+
+        Returns:
+            Table: A new object that is equivalent to the original.
+        """
         return Table(self.list.copy(), self.dict.copy())
-    def count(self, value: Any) -> int:
-        return self.list.count(value)
-    def foreach(self, func: Callable, /, list_eval: bool=True, dict_eval: bool=True):
+
+    def count(self, value: Any,  count_list: bool = True, count_dict_keys: bool = False, count_dict_values: bool = True) -> int:
+        """Counts the number of items in a list equal to value. Also counts the number of keys and values in the dict equal to value.
+
+        Args:
+            value (Any): The value to match for.
+            count_list (bool, optional): If list items should be checked. Defaults to True.
+            count_dict_keys (bool, optional): If dict keys should be checked. Defaults to False.
+            count_dict_values (bool, optional): If dict values should be checked. Defaults to True.
+
+        Returns:
+            int: _description_
+        """
+        ret = 0
+        ret += self.list.count(value) if count_list else 0
+        ret += list(self.dict.values()
+                    ).count(value) if count_dict_values else 0
+        ret += list(self.dict.keys()).count(value) if count_dict_keys else 0
+        return ret
+
+    def foreach(self, func: Callable, /, list_eval: bool =True, dict_eval: bool=True):
+        """Call a function on each item in the table.
+
+        Args:
+            func (Function): The function to call. Must take two arguments: key (or index), and value.
+            list_eval (bool, optional): Call the function on list items. Defaults to True.
+            dict_eval (bool, optional): Call the function on dict items. Defaults to True.
+
+        Returns:
+            List: A list of all of the function returns.
+            Any: The function return if there is only one.
+
+        Raises:
+            ValueError: If neither list_eval nor dict_eval are True.
+        """
+        if not (list_eval or dict_eval):
+            raise ValueError("There is nothing to evaluate!")
         ret = []
         if bool(list_eval):
             for i, j in enumerate(self.list):
-                ret += func(i, j) #type: ignore
+                ret += func(i, j)  # type: ignore
         if bool(dict_eval):
             for k, v in zip(self.dict.keys(), self.dict.values()):
-                ret += func(k, v) #type: ignore
+                ret += func(k, v)  # type: ignore
         return ret if len(ret) > 1 else ret[0] if len(ret) == 1 else None
+
     @override
     def __eq__(self, other):
         if isinstance(other, Table):
             return self.list == other.list and self.dict == other.dict
         return False
+
     def __add__(self, other: Any) -> "Table":
         if isinstance(other, Table):
             return Table(self.list + other.list, self.dict | other.dict)
@@ -186,8 +388,35 @@ class Table(Iterable):
         elif isinstance(other, dict):
             return Table(self.list, self.dict | other)
         else:
-            raise TypeError(f"unsupported operand type(s) for +: 'Table' and '{type(other).__name__}'")
+            raise TypeError(
+                f"unsupported operand type(s) for +: 'Table' and '{type(other).__name__}'")
+
     @override
-    def sort(self, key: Optional[Callable]=None, reverse: bool=False):
+    def sort(self, *, key: Optional[Callable] = None, reverse: bool=False) -> "Table":
+        """Sorts the list in place.
+
+        Args:
+            key (Optional[Callable], optional): The key to sort for. Defaults to None.
+            reverse (bool, optional): If you should sort in reverse. Defaults to False.
+
+        Returns:
+            Table: self. Useful for chaining and repls.
+        """
         self.list.sort(key=key, reverse=reverse)
         return self
+
+    @override
+    def __hash__(self) -> int:
+        return hash((self.list, self.dict))
+
+    @override
+    def __bool__(self) -> bool:
+        return bool(self.list) or bool(self.dict)
+
+    def isempty(self) -> bool:
+        """Returns True if the table is empty.
+
+        Returns:
+            bool: if the table is empty.
+        """
+        return not bool(self)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
+"""Unit tests for main.py
+"""
+
 from ..main import Table
-if __name__ == "__main__" and not __debug__: # definitions
+if __name__ == "__main__": # definitions
     print("Table definitions:")
     #print(f"{Table() = }")
     print(f"{repr(Table()) = }")
@@ -21,21 +24,29 @@ if __name__ == "__main__" and not __debug__: # definitions
     print(f"{Table(1,2,3).dict = }") # {}
     print(f"{Table(foo='bar', spam='eggs').list = }") # []
     print(f"{Table(foo='bar', spam='eggs').dict = }") # {'foo': 'bar', 'spam': 'eggs'}
-    print(f"{Table(1,2,3, foo='bar', spam='eggs').foreach((lambda _, y: y ), True, False) = }") # [1,2,3]
+    print(f"{Table(1,2,3, foo='bar', spam='eggs').foreach((lambda _, y: y ), True, False) = }")
     exit(0)
 
 def test_tables():
+    """Unit tests for Table() class
+    """
     assert True, "Test 0 failed!"
     assert repr(Table()) == "Table()", "Test 1 failed!"
     assert repr(Table(1, 2, 3)) == "Table([1, 2, 3])", "Test 2 failed!"
-    assert repr(Table(foo="bar", spam="eggs")) == "Table({'foo': 'bar', 'spam': 'eggs'})", "Test 3 failed!"
+    assert repr(
+        Table(foo="bar", spam="eggs")
+    ) == "Table({'foo': 'bar', 'spam': 'eggs'})", "Test 3 failed!"
     x = Table(1,2,3, foo="bar", spam="eggs")
     assert repr(x) == "Table([1, 2, 3]; {'foo': 'bar', 'spam': 'eggs'})", "Test 4 failed!"
     assert x.list == [1,2,3], "Test 5 failed!"
     assert x.dict == {"foo":"bar", "spam":"eggs"}, "Test 6 failed!"
     assert x == Table(1,2,3, foo="bar", spam="eggs"), "Test 7 failed!"
     assert x + Table(4,5,6) == Table(1,2,3,4,5,6, foo="bar", spam="eggs"), "Test 8 failed!"
-    assert x.foreach(lambda k, v: [k, v]) == [0, 1, 1, 2, 2, 3, "foo", "bar", "spam", "eggs"], "Test 9 failed!"
+    assert x.foreach(
+        lambda k, v: [k, v]) == [0, 1, 1, 2, 2, 3, "foo", "bar", "spam", "eggs"
+        ], "Test 9 failed!"
     assert Table(1,3,2).sort() == Table(1,2,3), "Test 10 failed!"
+    assert bool(Table(1,2,3)), "Test 11 failed!"
+    assert not bool(Table()), "Test 12 failed!"
     #congrats, the code works!
     print("All tests passed!")
